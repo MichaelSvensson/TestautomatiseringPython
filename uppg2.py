3abf542667aa4e68e95e6307ac23452842d15992
MichaelSvensson <michaelsdatakonsult@yahoo.se>
2025-01-13 05:30:59 +0100

    Added uppg2

---
+++ uppg2.py
@@ -0,0 +1,21 @@
+# x = 100  biljettpris
+# y = 200   pengar på fickan
+# print("Det blir " + (y - x) " kronor över.")
+# z = 200 - 100 / 2
+# print("Hälften är: " + z)
+
+# Correct code snippets
+
+# x = 100   biljettpris
+# y = 200   pengar på fickan
+# print("Det blir " + "(y - x)" + "kronor över.")
+# z = (200 - 100) / 2
+# print("Hälften är: ", z)
+
+# Better namnes on variables
+
+ticket_price = 100  # biljettpris
+pocket_money = 200  # pengar på fickan
+print("Det blir ", (pocket_money-ticket_price), "kronor över.")
+half = (200 - 100) / 2
+print("Hälften är: ", half)
