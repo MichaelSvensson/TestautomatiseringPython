c2ba01577034a860026ff1cdc57b8cdb3022850f
MichaelSvensson <michaelsdatakonsult@yahoo.se>
2025-01-13 04:48:40 +0100

    Added welcome file

---
+++ main.py
@@ -0,0 +1,55 @@
+print("Hello World!")
+print("Hi Michael")
+print("This program was made by Michael")
+
+# Variabler
+# greeting = "Good morning"
+# print(greeting)
+# name = "Michael"
+# message = "Hello " + name
+# print(message)
+
+# Värden och datatyper
+# intTal = 10
+# floatTal = 3.14
+# hiString = "hello"
+# tal = "15"
+# talFloat = float(tal)
+# print (talFloat)
+# Pi = 3.996
+# PiInt = int(Pi)
+# print(PiInt)
+# # -----------------------------------------
+# print("I have" + str(2) + "socks")
+# -----------------------------------------
+
+# This is a sample Python script.
+
+# Press Skift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+# Use a breakpoint in the code line below to debug your script.
+ #  print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+ #   print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
