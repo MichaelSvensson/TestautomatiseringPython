fc6f3a3179ee0b250612238cbc646daf555bc430
MichaelSvensson <michaelsdatakonsult@yahoo.se>
2025-01-16 10:36:49 +0100

    Added better print formatting

--- V01/uppg2.py
+++ V01/uppg2.py
@@ -13,9 +13,15 @@
 # print("Hälften är: ", z)
 
 # Better namnes on variables
+# ticket_price = 100  # biljettpris
+# pocket_money = 200  # pengar på fickan
+# print("Det blir ", (pocket_money-ticket_price), "kronor över.")
+# half = (200 - 100) / 2
+# print("Hälften är: ", half)
 
+# Formating the print command
 ticket_price = 100  # biljettpris
 pocket_money = 200  # pengar på fickan
-print("Det blir ", (pocket_money-ticket_price), "kronor över.")
+print(f"Det blir, {pocket_money-ticket_price}, kronor över.")
 half = (200 - 100) / 2
-print("Hälften är: ", half)
+print(f"Hälften är: , {half}")
