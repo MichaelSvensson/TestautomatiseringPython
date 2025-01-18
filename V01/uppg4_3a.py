6d9cfca9cae5dfaa01e08838a93bc6ab616fc2a1
MichaelSvensson <michaelsdatakonsult@yahoo.se>
2025-01-18 01:52:09 +0100

    Added uppg 4_3a.py

---
+++ V01/uppg4_3a.py
@@ -0,0 +1,13 @@
+# 4 - Fler övningar
+# 3a
+
+# Importerar klassen date från paketet datetime
+from datetime import date
+
+print("program som talar om dagens datum.")
+print("----------------------------------")
+
+print(date.today())
+
+
+
