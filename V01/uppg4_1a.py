44867973035476f395747e42cc6b43b92a99c5d2
MichaelSvensson <michaelsdatakonsult@yahoo.se>
2025-01-20 13:36:10 +0100

    Changed into sekunds again from hours

--- V01/uppg4_1a.py
+++ V01/uppg4_1a.py
@@ -1,7 +1,7 @@
 # 4 - Fler övningar
 # 1a
 
-print("1a - program som räknar ut hur lång tid det tar att köra från Stockholm till Göteborg.")
+print("1a - program som räknar ut hur lång tid i sekunder,det tar att köra från Stockholm till Göteborg.")
 print("--------------------------------------------------------------------------------------")
 s: int = 470000
 t: float = 0
@@ -10,9 +10,7 @@ x = input("Ange vilken medelhastighet (km/tim), du uppskattar hölls mellan Stoc
 if x.isnumeric():
     v = float(x)
     t = (470000 * 3.6) / v
-    # Omvandla till timmar
-    t = ((470000 * 3.6) / v)/3600
-    print(f"Det tar: {t} timmar, att köra {s} meter mellan Stockholm och Göteborg om man håller en medelhastighet av {v} km/tim.")
+    print(f"Det tar: {t} sekunder, att köra {s} meter mellan Stockholm och Göteborg om man håller en medelhastighet av {v} km/tim.")
 else:
     print("Endast heltal är tillåtet att mata in")
 
